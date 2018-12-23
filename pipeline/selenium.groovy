3acf20c8cedb0a227cb5af420ebbf5a799b46ce4
andanglyg@msn.com <Anthont@DESKTOP-UR7GL38>
2018-12-23 15:24:50 +0800

    add pipeline codes

---
+++ pipeline/selenium.groovy
@@ -0,0 +1,32 @@
+import hudson.model.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+
+def setKeyValue(key, value, file_path) {
+    Properties prop = new Properties()
+    try {
+        prop.load(new FileInputStream(file_path))
+    }catch (FileNotFoundException e){
+        e.printStackTrace()
+    }catch (IOException e) {
+        e.printStackTrace()
+    }
+    
+    // write into file 
+    try {
+        prop.setProperty(key, value)
+	    FileOutputStream fos = new FileOutputStream(file_path)
+        prop.store(fos, null)
+        fos.close()
+    }catch (FileNotFoundException e){
+        e.printStackTrace()
+    }catch (IOException e) {
+        e.printStackTrace()
+    }
+}
+
+return this;
---
+++ pipeline/selenium_jenkins.groovy
@@ -0,0 +1,63 @@
+import hudson.model.*;
+
+println env.JOB_NAME
+println env.BUILD_NUMBER
+
+
+pipeline{
+
+    agent any
+    parameters {
+        string(name: 'BROWSER_TYPE', defaultValue: 'chrome', description: 'Type a browser type, should be chrome/firefox')
+        string(name: 'TEST_SERVER_URL', defaultValue: '', description: 'Type the test server url')
+    }
+    
+	stages{
+	    stage("Initialization"){
+	        steps{
+	            script{
+	                selenium_test = load ${env.WORKSPACE} + "\\pipeline\\selenium.groovy"
+	            }
+	        }
+	    }
+
+	    stage("Git Checkout"){
+	        steps{
+	            script{
+	                checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+						userRemoteConfigs: [[credentialsId: '6f4fa66c-eb02-46dc-a4b3-3a232be5ef6e', 
+							url: 'https://github.com/QAAutomationLearn/JavaAutomationFramework.git']]])
+	            }
+	        }
+	    }
+	    
+        stage("Set key value"){
+	        steps{
+	            script{
+	                config_file = ${env.WORKSPACE} + "\\Config\\config.properties"
+	                selenium_test.setKeyValue("browser", "abc123", config_file)
+	            }
+
+	        }
+
+	    }
+	    stage("Run Selenium Test"){
+	        steps{
+	            script{
+	                println "Here will start to run selenium test."
+	            }
+
+	        }
+
+	    }
+	}
+
+
+
+
+
+
+
+
+
+}
