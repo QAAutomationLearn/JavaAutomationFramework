a29f10b8a234a01bdbecdd5671db17ce2600f02b
andanglyg@msn.com <Anthont@DESKTOP-UR7GL38>
2018-12-23 15:40:39 +0800

    add node in each stages

--- pipeline/selenium_jenkins.groovy
+++ pipeline/selenium_jenkins.groovy
@@ -10,13 +10,16 @@ pipeline{
     parameters {
         string(name: 'BROWSER_TYPE', defaultValue: 'chrome', description: 'Type a browser type, should be chrome/firefox')
         string(name: 'TEST_SERVER_URL', defaultValue: '', description: 'Type the test server url')
+        string(name: 'NODE', defaultValue: '', description: 'Please choose a windows node to execute this job.')
     }
     
 	stages{
 	    stage("Initialization"){
 	        steps{
 	            script{
-	                selenium_test = load ${env.WORKSPACE} + "\\pipeline\\selenium.groovy"
+	                node(NODE?.trim()) {
+	                    selenium_test = load ${env.WORKSPACE} + "\\pipeline\\selenium.groovy"
+	                }
 	            }
 	        }
 	    }
@@ -24,9 +27,11 @@ pipeline{
 	    stage("Git Checkout"){
 	        steps{
 	            script{
-	                checkout([$class: 'GitSCM', branches: [[name: '*/master']],
-						userRemoteConfigs: [[credentialsId: '6f4fa66c-eb02-46dc-a4b3-3a232be5ef6e', 
+	                node(NODE?.trim()) {
+	                     checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+						    userRemoteConfigs: [[credentialsId: '6f4fa66c-eb02-46dc-a4b3-3a232be5ef6e', 
 							url: 'https://github.com/QAAutomationLearn/JavaAutomationFramework.git']]])
+	                }
 	            }
 	        }
 	    }
@@ -34,29 +39,24 @@ pipeline{
         stage("Set key value"){
 	        steps{
 	            script{
-	                config_file = ${env.WORKSPACE} + "\\Config\\config.properties"
-	                selenium_test.setKeyValue("browser", "abc123", config_file)
+	                node(NODE?.trim()){
+	                    config_file = ${env.WORKSPACE} + "\\Config\\config.properties"
+	                    selenium_test.setKeyValue("browser", "abc123", config_file)
+	                }
 	            }
-
 	        }
-
 	    }
+	    
 	    stage("Run Selenium Test"){
 	        steps{
 	            script{
-	                println "Here will start to run selenium test."
+	                node(NODE?.trim()){
+	                    println "Here will start to run selenium test."
+	                }
 	            }
-
 	        }
-
 	    }
 	}
-
-
-
-
-
-
 
 
 
